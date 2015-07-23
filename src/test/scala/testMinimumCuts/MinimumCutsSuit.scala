6a54cb2680466d41a1d4ffbf6c04471f4ece3f21
GlulkAlex <alexanderglulkhovtsev@gmail.com>
2015-07-23 19:08:58 +0500

    Initial commit for the `randomized contraction algorithm` for the `min cut` problem

---
+++ src/test/scala/testMinimumCuts/MinimumCutsSuit.scala
@@ -0,0 +1,33 @@
+package testMinimumCuts
+
+import org.scalatest.FunSuite
+import minCutRandomContractionPQ3.MinimumCuts._
+/**
+ * Created by gluk-alex on 7/23/15.
+ */
+class MinimumCutsSuit
+  extends FunSuite {
+  ignore(
+          "1: 'getInput'" +
+            "should read from file"
+        ) {
+            //val inversionsNumber = new InversionsNumber
+            val unsorted: Array[Int] =
+              getInput()
+              .map(_.toInt)
+              .toArray
+            //Array(7, 5)
+            //Array(5, 7, 1)
+            //Array(5, 7, 1, 2)
+            //Array(5, 7, 9, 11, 2, 3, 4)
+
+            //println(s"unsorted was:${unsorted.take(10).mkString( """,""")}")
+            /*too large for integer*/
+            assume(
+                    //true == true,
+                    unsorted.length == 10000,
+                    "must be equal"
+                  )
+          }
+
+}
